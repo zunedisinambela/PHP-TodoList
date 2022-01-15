613594582adc9c94b21972a6c3235b7c87abf0aa
zunedisinambela <zunedisinambela@gmail.com>
2022-01-15 19:45:09 +0700

    Membuat View

--- App.php
+++ App.php
@@ -4,5 +4,8 @@ require_once "Model/TodoList.php";
 require_once "BusinessLogic/ShowTodoList.php";
 require_once "BusinessLogic/AddTodoList.php";
 require_once "BusinessLogic/RemoveTodoList.php";
+require_once "View/ViewShowTodoList.php";
+require_once "View/ViewAddTodoList.php";
+require_once "View/ViewRemoveTodoList.php";
 
 echo "Aplikasi TodoList" . PHP_EOL;
