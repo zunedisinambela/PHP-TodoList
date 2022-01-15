cfbe0dc7d8c326db0df5a32d780cf6585afe02b8
zunedisinambela <zunedisinambela@gmail.com>
2022-01-15 22:09:05 +0700

    Test Remove TodoList

---
+++ Test/TestRemoveTodoList.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+
+addTodoList("Zunedi");
+addTodoList("Suprianto");
+addTodoList("Sinambela");
+addTodoList("Budi");
+addTodoList("Santoso");
+
+showTodoList();
+
+removeTodoList(3);
+
+showTodoList();
+
+removeTodoList(2);
+
+showTodoList();
+
+$success = removeTodoList(4);
+var_dump($success);
+
+showTodoList();
