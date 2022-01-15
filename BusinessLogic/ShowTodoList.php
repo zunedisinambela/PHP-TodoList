ea2d93e302de3d0d3019d00a37c86e50c2d55f85
zunedisinambela <zunedisinambela@gmail.com>
2022-01-15 20:44:38 +0700

    Menampilkan TodoList

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -5,5 +5,11 @@
  */
 function showTodoList()
 {
-    //
+    global $todoList;
+
+    echo "TODOLIST" . PHP_EOL;
+
+    foreach ($todoList as $number => $value) {
+        echo "$number. $value" . PHP_EOL;
+    }
 }
