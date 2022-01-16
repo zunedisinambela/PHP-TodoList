785f28cdcea9849b72c9d2207e83c1a5e331c172
zunedisinambela <zunedisinambela@gmail.com>
2022-01-16 21:08:16 +0700

    View Menambahkan TodoList

--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -1,6 +1,18 @@
 <?php
 
+require_once "../Model/TodoList.php";
+require_once "../Helper/Input.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
 function viewAddTodoList()
 {
-    //
+    echo "MENAMBAH TODOLIST" . PHP_EOL;
+
+    $todo = input("Todo (x untuk batal) : ");
+
+    if ($todo == "x") {
+        // batal
+    } else {
+        addTodoList($todo);
+    }
 }
