a2b27485da3babe9dc0ef6f659b7292388f22c44
zunedisinambela <zunedisinambela@gmail.com>
2022-01-16 21:00:24 +0700

    Test View Show TodoList

---
+++ Test/TestViewShowTodoList.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../View/ViewShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+addTodoList("Zunedi");
+addTodoList("Suprianto");
+addTodoList("Sinambela");
+
+viewShowTodoList();
--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,8 +1,9 @@
 <?php
 
+require_once "../Model/TodoList.php";
 require_once "../BusinessLogic/ShowTodoList.php";
-require_once "../BusinessLogic/AddTodoList.php";
-require_once "../BusinessLogic/RemoveTodoList.php";
+require_once "../View/ViewAddTodoList.php";
+require_once "../View/ViewRemoveTodoList.php";
 require_once "../Helper/Input.php";
 
 function viewShowTodoList()
