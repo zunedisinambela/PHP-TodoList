a721a553c23274778f67cde05865f30fe4479ce1
zunedisinambela <zunedisinambela@gmail.com>
2022-01-16 19:03:39 +0700

    View Menampilkan TodoList

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,6 +1,32 @@
 <?php
 
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+require_once "../Helper/Input.php";
+
 function viewShowTodoList()
 {
-    //
+    while (true) {
+        showTodoList();
+
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = input("Pilih");
+
+        if ($pilihan == "1") {
+            viewAddTodoList();
+        } else if($pilihan == "2") {
+            viewRemoveTodoList();
+        } else if($pilihan == "x") {
+            break;
+        } else {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
